c2812641d8a029a879385229a77bfd11d24afd95
ronroeandassociates <3321@holbertonschool.com>
2021-10-05 10:44:25 -0700

    define nothing

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""module to create a class named square"""
-
-
 class Square:
-    """ class of a quadrilateral with out type or value"""
-
     def __init__(self, size);
         self.__size = size
