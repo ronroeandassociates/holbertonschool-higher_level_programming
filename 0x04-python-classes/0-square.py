5c56a9394c441680471be8d35ee96932a252daca
ronroeandassociates <3321@holbertonschool.com>
2021-10-05 11:04:59 -0700

    define nothing

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# module for doing square things
+"""module for doing square things"""
 
 class Square:
-    # define the zise of the eraqs
+    """define the zise of the eraqs"""
 
 def __init__(self, size);
+    """more definition here"""
    self.__size = size
