74242657d7b59d595313648741920b15343aa027
ronroeandassociates <3321@holbertonschool.com>
2021-10-05 16:58:04 -0700

    amazing the difference between a colon and a semicolon

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """module for doing square things"""
 
+
 class Square:
     """a fancy quadrilateral with 90 degree corners and equal sides"""
 
+
 def __init__(self, size):
-    """done in a private mode"""
     self.__size = size        
