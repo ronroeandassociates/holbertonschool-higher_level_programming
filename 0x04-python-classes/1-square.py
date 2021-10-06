760646e6c72ae1a82519f8cb14c8f60323c57c37
ronroeandassociates <3321@holbertonschool.com>
2021-10-05 17:17:01 -0700

    Write a class Square that defines a square by: (based on 0-square.py)
    Private instance attribute: size
    Instantiation with optional size: def __init__(self, size=0):
    size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    if size is less than 0, raise a ValueError exception with the message size must be >= 0
    You are not allowed to import any module

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-""" make a class square with some size """
+""" make a module with a class of square """
 
 
 class Square:
-    """make it all about size"""
-    def __init__(self, size)
+    """make it all about size and some private attributes"""
+    def __init__(self, size=0)
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
         self.__size = size
