0f5374292076963da066361ddc80b40281b4e6c6
ronroeandassociates <3321@holbertonschool.com>
2021-10-06 04:39:24 -0700

    syntax syntax

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or isinstance(roman_string, str) is False:
+    letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000}
+    ival = 0
+    rome = roman_string
+    if type(roman_string) != str or roman_string is None:
         return 0
-    letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    reverse_num = roman_string[::-1]
-    maxchar = 'I'
-    total = 0
-    for i in reverse_num:
-        if letters[i] >= letters[maxchar]:
-        maxchar = i
-        total += letters[i]
-    else:
-        total -= letters[i]
-    return total
+    for i in range(len(roman_string)):
+        if i > 0 and letters[rome[i]] > letters[rome[i-1]]:
+            iVal += letters[rome[i]] - \
+                    2 * letters[rome[i-1]]
+        else:
+            ival += letters[rome[i]]
+    return ival            
