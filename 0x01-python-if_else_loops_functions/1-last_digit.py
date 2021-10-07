c99fe1dc06d2e2d0e0a115b6eef7b9c4572298ef
ronroeandassociates <3321@holbertonschool.com>
2021-10-07 08:56:58 -0700

    uploaded task 0-5 to the repository 0. Positive anything is better than negative nothing uses a random number using given source code then right pos neg zero 1. The last digit actually wrote before in 0x00 2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game did it in C now in Python 3. When I was having that alphabet soup, I never thought that it would pay off I will just say ibid 4. Hexadecimal printing did this in 0x00 5. 00...99 ibid 6. Inventing is a combination of brains and materials. The more brains you use, the less material you need ibid

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+modnum = number % 10 if number > 0 else number % -10
+if modnum > 5:
+    print("Last digit of {} is {} and is greater than 5".format(
+        number, modnum))
+elif modnum == 0:
+    print("Last digit of {} is {} and is 0".format(
+        number, modnum))
+else:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(
+        number, modnum))
