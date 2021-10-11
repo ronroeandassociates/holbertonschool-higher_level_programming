2494baf8b09ccf98b78175b2b44306bea2e383b3
ronroeandassociates <3321@holbertonschool.com>
2021-10-11 16:25:18 -0700

    removed lines that helped with reading to help with pep8

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -23,7 +23,6 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
     @width.setter
     def width(self, value):
         if type(value) is not int:
@@ -32,11 +31,9 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-    
     @property
     def height(self):
         return self.__height
-    
     @height.setter
     def height(self, value):
         if type(value) is not int:
