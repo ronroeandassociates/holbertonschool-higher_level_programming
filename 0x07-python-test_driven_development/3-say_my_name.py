3c604d08575c9b38a38546e8cf881bf1f0b49aea
ronroeandassociates <3321@holbertonschool.com>
2021-10-12 12:11:04 -0700

    fixed some syntax

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -13,8 +13,8 @@ def say_my_name(first_name, last_name=""):
         Raise:
             TypeError: if first or last name is not string
         Return: print My name is <first name> <last name>
-        
         """
+
     if type(first_name) is not str:
         raise TypeError('first_name must be a string')
     if type(last_name) is not str:
