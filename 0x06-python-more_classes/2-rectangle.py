228573eed160b83c370da87e4aaa6fb6d42d9401
ronroeandassociates <3321@holbertonschool.com>
2021-10-11 17:32:29 -0700

    fixed a bit of syntax

--- 0x06-python-more_classes/2-rectangle.py
+++ 0x06-python-more_classes/2-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
 
     def area(self):
         return self.__width * self.__height
-    
+
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return 0
