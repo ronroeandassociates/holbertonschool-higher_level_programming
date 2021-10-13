2f0d649b4233344c7dd34e8cbb334d4651ac18d4
ronroeandassociates <3321@holbertonschool.com>
2021-10-13 16:55:48 -0700

    got to work on my tabs

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,7 +4,6 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
-
 class Square(Rectangle):
     """Creating the class Square"""
 
@@ -12,7 +11,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-    
+
     def area(self):
         return self.__size * self.__size
 
