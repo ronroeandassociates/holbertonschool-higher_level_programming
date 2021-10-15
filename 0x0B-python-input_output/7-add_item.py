ad01a182df13c52184a9a74ee1535d7650fce8c7
ronroeandassociates <3321@holbertonschool.com>
2021-10-15 15:16:58 -0700

    it is all in the details

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,6 +2,7 @@
 """ add stuff"""
 
 import sys
+import json
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
