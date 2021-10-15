0a87c064bd810c6af2b8438a5add80c57296c79d
ronroeandassociates <3321@holbertonschool.com>
2021-10-15 15:00:40 -0700

    let's try a few things out

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" lets write a file """
+"""
+lets write a file
+"""
 
 
 def write_file(filename="", text=""):
-    """ read the file and write it """
-    
+    """
+    read the file and write it
+    """
     with open(filename, "w") as writefile:
         writefile.write(text)
     return (len(text))
