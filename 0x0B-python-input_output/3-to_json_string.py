b74021088dc2cdc93909ea84a9d49f750b82be6b
ronroeandassociates <3321@holbertonschool.com>
2021-10-15 15:11:01 -0700

    does a little space really mean that much

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""git json"""
+"""
+    to json
+    """
 import json
 
 
 def to_json_string(my_obj):
     """return json format of my_obj"""
-    
     return (json.dumps(my_obj))
