31a891e32d09de62b4b0a2f8cce31ea6dbd6347a
ronroeandassociates <3321@holbertonschool.com>
2021-10-20 17:26:51 -0700

    needed a setter to go with a getter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,3 +16,52 @@ class Rectangle(Base):
         @property
         def width(self):
             return self.__width
+
+        @width.setter
+        def width(self, value):
+            if type(value) != int:
+                raise TypeError("width must be an integer")
+            if value <= 0:
+                raise ValueError("width must be > 0")
+            self.width = value
+
+        @property
+        def height(self):
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            if type(value) != int:
+                raise TypeError("height must be an integer")
+            if value <= 0:
+                raise ValueError("height myst be > 0")
+            self.__height = value
+
+        @property
+        def x(self):
+            return self.__x
+
+        @x.setter
+        def x(self, value):
+            if type(value) != int:
+                raise TypeError("x must be an integer")
+            if value <= 0:
+                raise ValueError("x must be >= 0")
+            self.__x = value
+
+        @property
+        def y(self):
+            return self.__y
+
+        @y.setter
+        def y(self, value):
+            if type(value) != int:
+                raise TypeError("y must be an integer")
+            if value <= 0:
+                raise ValueError("y must be >=0")
+            self.__y = value
+
+        def area(self):
+            """area of rectangle"""
+
+            return self.__width * self.__height
