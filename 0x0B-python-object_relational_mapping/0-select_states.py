d39771f361023659a4207b9557f4b3430d7c7dd1
Ronroeandassociates <3321@holbertonschool.com>
2021-11-16 12:29:03 -0600

    moved if statement to line 20

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,21 @@
 """Module lists all states from the database hbtn_0e_0_usa"""
 
+import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+# default values user="root", passwd="", db="hbtn_0e_0_usa"
+db = MySQLdb.connect(host="localhost",
+                      port=3306,
+                      user=argv[1],
+                      passwd=argv[2],
+                      db=argv[3])
+ cur = db.cursor()
+  cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-    # default values user="root", passwd="", db="hbtn_0e_0_usa"
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-
-    for row in cur.fetchall():
+   for row in cur.fetchall():
         print(row)
     db.close()
+
+
+if __name__ == "__main__":
+    main()
