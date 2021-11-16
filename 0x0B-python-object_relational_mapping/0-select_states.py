3951077e022415e14b3d0ded1dd3a367a67c26f1
Ronroeandassociates <3321@holbertonschool.com>
2021-11-16 12:45:28 -0600

    adding a few small details

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,22 @@
 """Module lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
-import sys
+from sys import argv
+
+
+def main():
 
 # default values user="root", passwd="", db="hbtn_0e_0_usa"
-db = MySQLdb.connect(host="localhost",
-                      port=3306,
+    db = MySQLdb.connect(host="localhost",
+                    port=3306,
                       user=argv[1],
                       passwd=argv[2],
                       db=argv[3])
- cur = db.cursor()
-  cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur = db.cursor()
 
-   for row in cur.fetchall():
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    for row in cur.fetchall():
         print(row)
     db.close()
 
