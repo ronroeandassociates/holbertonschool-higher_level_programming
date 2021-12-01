b29c1c4013cd95dd1db466c0fc662f7171590dd0
Ronroeandassociates <3321@holbertonschool.com>
2021-12-01 11:23:17 -0600

    script that fetches

---
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status
+using requests"""
+
+import requests
+
+if __name__ == "__main__":
+
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("	- type: {}".format(type(req.text)))
+    print("	- content: {}".format(req.text))
