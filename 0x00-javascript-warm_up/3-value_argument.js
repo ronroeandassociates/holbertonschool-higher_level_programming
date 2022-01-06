478e6319e1229c1e19523bcc55ce703478638e36
Ronroeandassociates <3321@holbertonschool.com>
2022-01-06 10:11:12 -0600

    script that prints the first argument passed to it:
    If no arguments are passed to the script, print “No argument”
    You must use console.log(...) to print all output
    You are not allowed to use var
    You are not allowed to use length

--- 0x00-javascript-warm_up/3-value_argument.js
+++ 0x00-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+let x = 0;
+process.argv.forEach((val, index) => {
+    x++;
+    if (index === 2) {
+        console.log(`${val}`);
+    }
+});
+if (x <= 2) {
+    console.log('No argument');
+}
