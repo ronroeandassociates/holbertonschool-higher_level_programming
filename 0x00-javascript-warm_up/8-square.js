4e83f062146eb5e412088a7cc97e55837764bed6
Ronroeandassociates <3321@holbertonschool.com>
2022-01-06 13:48:22 -0600

    cleaned up some stuff

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -3,12 +3,12 @@
 const MyVar = process.argv[2];
 let A = '';
 if (isNaN(MyVar) === true) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let i = 0; i < MyVar; i++) {
-        A += 'X';
-    }
-    for (let i = 0; i < MyVar; i++) {
-        console.log(A);
-    }
+  for (let i = 0; i < MyVar; i++) {
+    A += 'X';
+  }
+  for (let i = 0; i < MyVar; i++) {
+    console.log(A);
+  }
 }
