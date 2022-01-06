c269e1fac03c16561b634a891dd08c48fcb67c8c
Ronroeandassociates <3321@holbertonschool.com>
2022-01-06 12:00:12 -0600

    script that prints x times “C is fun”

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+/* script that prints x times “C is fun” */
+
+const MyVar = process.argv[2];
+if (isNaN(MyVar) === true) {
+    console.log('Missing number of occurrences');
+} else {
+    for (let i = 0; i < MyVar; i++) {
+        console.log('C is fun');
+    }
+}
