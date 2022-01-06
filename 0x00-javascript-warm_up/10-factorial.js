6d584424cfe5acf4f6460b5c5b9963b75d729d97
Ronroeandassociates <3321@holbertonschool.com>
2022-01-06 13:49:55 -0600

    script that computes and prints a factorial
    The first argument is integer (argument can be cast as integer) used for computing the factorial
    Factorial of NaN is 1
    You must do it recursively
    You must use a function
    You must use console.log(...) to print all output
    You are not allowed to use var

--- 0x00-javascript-warm_up/10-factorial.js
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
-function factorialize(num) {
-    let MyRes = 1;
-    for (let i = 1; i <= num; i++) {
-        MyRes *= i;
-    }
-    return (MyRes);
+function factorialize (num) {
+  let MyRes = 1;
+  for (let i = 1; i <= num; i++) {
+    MyRes *= i;
+  }
+  return (MyRes);
 }
 console.log(factorialize(parseInt(process.argv[2])));
