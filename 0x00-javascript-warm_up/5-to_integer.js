df75ff33f120850c058508329727a86560f15df0
Ronroeandassociates <3321@holbertonschool.com>
2022-01-06 10:41:50 -0600

    script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
    If the argument can’t be converted to an integer, print “Not a number”
    You must use console.log(...) to print all output
    You are not allowed to use var
    You are not allowed to use try/catch

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+const MyVar = process.argv[2];
+if (isNaN(parseInt(MyVar)) === true) {
+    console.log('Not a number');
+} else {
+    console.log('My number: ' + parseInt(MyVar));
+}
