1d2e78f1fea3ccb0349c72cede269f637f88e146
Ronroeandassociates <3321@holbertonschool.com>
2022-01-06 13:17:10 -0600

    try to fix it

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const MyVar = process.argv[2];
 if (isNaN(MyVar) === true) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < MyVar; i++) {
-    console.log('C is fun');
+for (let i = 0; i < MyVar; i++) {
+  console.log('C is fun');
     }
 }
