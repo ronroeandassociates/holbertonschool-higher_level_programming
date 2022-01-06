c7c2cf9b58c4f5ecaeeebf7cbafa404a8a2666ed
Ronroeandassociates <3321@holbertonschool.com>
2022-01-06 13:02:56 -0600

    removed lines and space

--- 0x00-javascript-warm_up/3-value_argument.js
+++ 0x00-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-
 let x = 0;
 process.argv.forEach((val, index) => {
-    x++;
-    if (index === 2) {
-        console.log(`${val}`);
-    }
+  x++;
+  if (index === 2) {
+      console.log(`${val}`);
+  }
 });
 if (x <= 2) {
-    console.log('No argument');
+  console.log('No argument');
 }
