4241a5cb06c1daf715a9e986d19d19bb6536e16f
Ronroeandassociates <3321@holbertonschool.com>
2022-01-06 13:05:36 -0600

    removed extra spaces

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const MyVar = process.argv[2];
 if (isNaN(parseInt(MyVar)) === true) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + parseInt(MyVar));
+  console.log('My number: ' + parseInt(MyVar));
 }
